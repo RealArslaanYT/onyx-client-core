ed471baab7a7385dad5961b9079396693efd612f
Arslaan Pathan <xminecrafterfun@gmail.com>
2025-04-24 21:12:06 +1200

    add custom main menu

--- src/main/java/com/onyxclient/onyxclientcore/OnyxClient_Client.java
+++ src/main/java/com/onyxclient/onyxclientcore/OnyxClient_Client.java
@@ -1,10 +1,11 @@
 package com.onyxclient.onyxclientcore;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.MinecraftClient;
 
 public class OnyxClient_Client implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        MinecraftClient.getInstance().setScreen(new CustomMainMenuScreen());
     }
 }
