394c0a38894912e1e817946accb93403aa59b6e6
Arslaan Pathan <arslaanpathan@Arslaans-MacBook-Pro.local>
2025-04-24 21:01:02 +1200

    Initial commit

---
+++ src/main/java/com/onyxclient/onyxclientcore/OnyxClientCoreDataGenerator.java
@@ -0,0 +1,11 @@
+package com.onyxclient.onyxclientcore;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OnyxClientCoreDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/com/onyxclient/onyxclientcore/OnyxClient_Client.java
@@ -0,0 +1,10 @@
+package com.onyxclient.onyxclientcore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OnyxClient_Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
